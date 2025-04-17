5401fbcab332d8fe6c9ec2e7b6546a90859afe90
sajid-49 <ahmedsajid.taradin07@gmail.com>
2025-04-17 21:34:36 +0600

    banner section done

--- script/index.js
+++ script/index.js
@@ -7,6 +7,12 @@ function loadCategory() {
     .then((data) => displaycategories(data.categories));
 }
 
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos));
+}
+
 function displaycategories(categories) {
   // get the container
   const categoryContainer = document.getElementById("category-container");
@@ -27,4 +33,55 @@ function displaycategories(categories) {
   }
 }
 
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  videos.forEach((video) => {
+    // console.log(video);
+
+    const videoCard = document.createElement("div");
+
+    videoCard.innerHTML = `
+     <div class="card bg-base-100">
+        <figure class="relative">
+          <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+          <span
+            class="absolute bottom-2 right-2 text-white bg-black px-2 text-sm rounded"
+            >3hrs 56 min ago</span
+          >
+        </figure>
+
+        <div class="flex gap-3 px-0 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div
+                class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2"
+              >
+                <img
+                  src="${video.authors[0].profile_picture}"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="intro">
+            <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+            <p class="text-sm text-gray-400 flex gap-1">
+              ${video.authors[0].profile_name}
+              <img
+                class="w-5 h-5"
+                src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png"
+                alt=""
+              />
+            </p>
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+        </div>
+      </div>
+    `;
+    videoContainer.append(videoCard);
+  });
+};
+
 loadCategory();
+loadVideos();
